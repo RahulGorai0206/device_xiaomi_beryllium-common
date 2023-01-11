d24d501584327d494efe0169f40fdee2fe817ecd
karan2354 <karann2354@users.noreply.github.com>
2023-01-11 19:15:40 +0530

    sdm845-common: Build aidl power stats * Fix for aidl power stats errors. * New updated legacy-um sepolicy solved the android.hardware.power@1.0::IPower/default error - Now with this we fixing both errors: BatteryStatsService: Unable to load Power Hal or power.stats HAL hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest. hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.

--- sdm845.mk
+++ sdm845.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
